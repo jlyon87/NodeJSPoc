b11103acb494fb9b929f096679a549f25ba9c270
jlyon87 <jml6487@gmail.com>
2016-06-13 18:29:16 -0400

    end of input

--- app/js/EVECREST.js
+++ app/js/EVECREST.js
@@ -4,9 +4,10 @@ function querySystem() {
 		$.each( data, function( key, val ) {
 			items.push( "<li id='" + key + "'>" + val + "</li>" );
 		});
-		
+
 		$("<ul/>", {
 			"class": "my-new-list",
 			html: items.join( "" )
 		}).appendTo( "body" );	
-});
+	});
+}
