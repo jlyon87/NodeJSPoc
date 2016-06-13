a9d39d7a1982f7f86ee26608f6b7fc4dcc1dd357
Justin Lyon <jml6487@gmail.com>
2016-06-13 15:46:08 -0400

    Goonies HTTP

--- app/js/MoviePoster.js
+++ app/js/MoviePoster.js
@@ -19,12 +19,12 @@ $(document).ready(function(){
 
             $('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
 
-            $.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/
+            $.getJSON("//api.themoviedb.org/2.1/Movie.search/en/json/
 23afca60ebf72f8d88cdcae2c4f31866/" + film + "?callback=?", function(json) {
                if (json != "Nothing found."){
                      $('#poster').html('<h2 class="loading">Well, gee whiz! We found you a poster, skip!</h2><img id="thePoster" src=' + json[0].posters[0].image.url + ' />');
                   } else {
-                     $.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/
+                     $.getJSON("//api.themoviedb.org/2.1/Movie.search/en/json/
 23afca60ebf72f8d88cdcae2c4f31866/goonies?callback=?", function(json) {
                         console.log(json);
                         $('#poster').html('<h2 class="loading">We're afraid nothing was found for that search. Perhaps you were looking for The Goonies?</h2><img id="thePoster" src=' + json[0].posters[0].image.url + ' />');
