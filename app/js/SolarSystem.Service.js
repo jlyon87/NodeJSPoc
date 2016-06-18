94055512ff7bce542932afbe5f039a0c8a0547cf
jlyon87 <jml6487@gmail.com>
2016-06-18 09:17:48 -0400

    Revert "Revert "SolarSystemList""
    This reverts commit 2a6fa70895b5ce9deb74d2b7fb960c3fd533bc07.

---
+++ app/js/SolarSystem.Service.js
@@ -0,0 +1,25 @@
+var SolarSystem = (function(SolarSystem)) {
+
+	SolarSystem.Service = {
+		queryAllSystems: queryAllSystems
+	};
+
+	return SolarSystem;
+
+	function queryAllSystems() {
+		$.getJSON( "https://crest-tq.eveonline.com/solarsystems/", function(data) {
+			/*var items = [];
+			$.each( data, function( key, val ) {
+				items.push( "<li id='" + key + "'>" + val + "</li>" );
+				console.log(key);
+				console.log(val);
+			});
+
+			$("<ul/>", {
+				"class": "my-new-list",
+				html: items.join( "" )
+			}).appendTo( "body" );	*/
+			return data;
+		});
+	}
+}
